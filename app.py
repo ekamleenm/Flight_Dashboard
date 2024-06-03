855caba40a7c915c469f3308c9f528dee4e163b4
ekamleen <ekamleenm@gmail.com>
2024-06-03 14:24:08 -0700

    Dashboard UI

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-import streamlit
+import streamlit as st
+
+st.sidebar.title('Flight Analytics')
+user_option = st.sidebar.selectbox('Menu', ['Select One', 'Check Flights', 'Analytics'])
+
+if user_option == 'Check Flights':
+    st.title('Flights')
+elif user_option == 'Analytics':
+    st.title("Analytics")
+else:
+    st.title('About the App')
